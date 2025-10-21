ac134f3f6402044bfb0f44bfb67ef1aef9e824b2
HQHuy1312 <huy.huynh@baoinsure.ai>
2025-10-21 20:39:57 +0700

    Fix primary key column name in TiktokShopTokens table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE TiktokShopTokens (
-    customer_id VARCHAR(100) PRIMARY KEY,
+    shop_id VARCHAR(100) PRIMARY KEY,
     access_token TEXT NOT NULL,
     access_token_expire_in BIGINT NOT NULL,
     refresh_token TEXT NOT NULL,

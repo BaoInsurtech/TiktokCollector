336f312689ccf2dac69f6c5650cd63e0ab344073
HQHuy1312 <huy.huynh@baoinsure.ai>
2025-10-22 13:46:09 +0700

    finish flow auth

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -5,14 +5,15 @@ from prisma import Prisma
 from model.common_model import APIResponse
 from model.auth_model import AuthTokenResponse
 
+APP_KEY = os.getenv("APP_KEY", "")
 APP_SECRET_KEY = os.getenv("APP_SECRET_KEY", "")
 
 async def auth_callback_handler(app_key: str = None, code: str = None) -> APIResponse:
-    if app_key == None or code == None:
+    if code == None:
         return APIResponse(code=400, message="Invalid parameters")
     try:
         params = {
-            "app_key": app_key,
+            "app_key": APP_KEY,
             "app_secret": APP_SECRET_KEY,
             "auth_code": code,
             "grant_type": "authorized_code"
@@ -27,3 +28,42 @@ async def auth_callback_handler(app_key: str = None, code: str = None) -> APIRes
         return APIResponse(code=500, message="Internal server error")
     return APIResponse(code=200, message="Authorization successful")
 
+async def refresh_token_handler():
+    refresh_token = None
+    try:
+        tmp = await Prisma.TiktokShopTokens.get(
+            where={
+                "id": 1
+            },
+            select={
+                "refresh_token": True
+            }
+        )
+        refresh_token = tmp.refresh_token
+    except Exception as e:
+        print(f"Error fetching refresh token: {e}")
+        return APIResponse(code=500, message="Internal server error")
+
+    if refresh_token is None:
+        return APIResponse(code=400, message="No refresh token found")
+    
+    try:
+        params = {
+            "app_key": APP_KEY,
+            "app_secret": APP_SECRET_KEY,
+            "refresh_token": refresh_token,
+            "grant_type": "authorized_code"
+        }
+        response = requests.get("https://auth.tiktok-shops.com/api/v2/token/refresh", params=params)
+        response.raise_for_status()
+        data: AuthTokenResponse = response.json().get("data", {})
+        await Prisma.TiktokShopTokens.update({
+            "where": {
+                "id": 1
+            },
+            "data": data.model_dump()
+        })
+        return APIResponse(code=200, message="Authorization successful")
+    except Exception as e:
+        print(f"Error in auth_callback_handler: {e}")
+        return APIResponse(code=500, message="Internal server error")
--- src/routes/auth_route.py
+++ src/routes/auth_route.py
@@ -7,5 +7,7 @@ def register_auth_routes(app):
     @router.get("/callback", tags=["auth"])
     async def callback(app_key: str = None, code: str = None):
         return await auth_callback_handler(app_key, code)
-
+    @router.post("/refresh-token", tags=["auth"])
+    async def refresh_token():
+        return await refresh_token_handler()
     app.include_router(router, prefix="/auth")
